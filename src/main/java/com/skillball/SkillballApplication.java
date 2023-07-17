34d0a6c1baa484efbd1a2d09068fae79409c04b6
st155220 <33095211+st155220@users.noreply.github.com>
2023-07-17 09:01:14 +0200

    Hello removed

--- src/main/java/com/skillball/SkillballApplication.java
+++ src/main/java/com/skillball/SkillballApplication.java
@@ -3,7 +3,6 @@ package com.skillball;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Hello
 @SpringBootApplication
 public class SkillballApplication {
     public static void main(String[] args) {
