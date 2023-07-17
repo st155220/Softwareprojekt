41c8cbcddcb8b338bb0aa08a99d8183354e66cf6
st155220 <33095211+st155220@users.noreply.github.com>
2023-07-17 08:59:50 +0200

    Update SkillballApplication.java
    Hello eingefügt

--- src/main/java/com/skillball/SkillballApplication.java
+++ src/main/java/com/skillball/SkillballApplication.java
@@ -3,6 +3,7 @@ package com.skillball;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Hello
 @SpringBootApplication
 public class SkillballApplication {
     public static void main(String[] args) {
